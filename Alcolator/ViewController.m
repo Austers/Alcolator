18fa1f801b4196489ddd8d0710951f801587990b
Richie Austerberry <rtausterberry@gmail.com>
2014-10-08 15:43:44 +0800

    Wrote a custom view controller

--- Alcolator/ViewController.m
+++ Alcolator/ViewController.m
@@ -8,17 +8,44 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
-@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
-@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-@property (weak, nonatomic) IBOutlet UILabel *beerLabel;
+@interface ViewController () <UITextFieldDelegate>
 
+@property (weak, nonatomic) UIButton *calculateButton;
+@property (weak, nonatomic) UITapGestureRecognizer *hideKeyboardTapGestureRecognizer;
 @end
 
 @implementation ViewController
 
-- (IBAction)textFieldDidChange:(UITextField *)sender {
+-(void)loadView {
+
+    self.view = [[UIView alloc]init];
+    
+    UITextField *textField = [[UITextField alloc]init];
+    UISlider *slider = [[UISlider alloc]init];
+    UILabel *label = [[UILabel alloc]init];
+    UILabel *bLabel = [[UILabel alloc]init];
+    UIButton *button = [[UIButton alloc]init];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]init];
+    
+    [self.view addSubview:textField];
+    [self.view addSubview:slider];
+    [self.view addSubview:label];
+    [self.view addSubview:bLabel];
+    [self.view addSubview:button];
+    [self.view addGestureRecognizer:tap];
+    
+    self.beerPercentTextField = textField;
+    self.beerCountSlider = slider;
+    self.resultLabel = label;
+    self.beerLabel = bLabel;
+    self.calculateButton = button;
+    self.hideKeyboardTapGestureRecognizer = tap;
+
+}
+
+
+
+- (void)textFieldDidChange:(UITextField *)sender {
     
     NSString *enteredText = sender.text;
     float enteredNumber = [enteredText floatValue];
@@ -29,7 +56,7 @@
     }
     
 }
-- (IBAction)sliderValueDidChange:(UISlider *)sender {
+- (void)sliderValueDidChange:(UISlider *)sender {
     
     NSLog(@"Slider value changed to @f", sender.value);
     [self.beerPercentTextField resignFirstResponder];
@@ -40,7 +67,7 @@
     self.beerLabel.text = beerNumber;
     
 }
-- (IBAction)buttonPressed:(UIButton *)sender {
+- (void)buttonPressed:(UIButton *)sender {
     
     [self.beerPercentTextField resignFirstResponder];
     
@@ -78,7 +105,7 @@
 
     
 }
-- (IBAction)tapGestureDidFire:(UITapGestureRecognizer *)sender {
+- (void)tapGestureDidFire:(UITapGestureRecognizer *)sender {
     
     [self.beerPercentTextField resignFirstResponder];
     
@@ -87,7 +114,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor brownColor];
+    
+    self.beerPercentTextField.delegate = self;
+    self.beerPercentTextField.placeholder = NSLocalizedString(@"% Alcohol Content per Beer", @"Beer percent placeholder text");
+    self.beerPercentTextField.font = [UIFont fontWithName:@"Times New Roman" size:20];
+    self.beerPercentTextField.textColor = [UIColor greenColor];
+    self.beerPercentTextField.backgroundColor = [UIColor clearColor];
+    
+    
+    [self.beerCountSlider addTarget:self action:@selector(sliderValueDidChange:) forControlEvents:UIControlEventValueChanged];
+    
+    self.beerCountSlider.minimumValue = 1;
+    self.beerCountSlider.maximumValue = 10;
+    
+    [self.calculateButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.calculateButton setTitle:NSLocalizedString(@"Calculate!", @"Calculate command") forState:UIControlStateNormal];
+    
+    [self.hideKeyboardTapGestureRecognizer addTarget:self action:@selector(tapGestureDidFire:)];
+    
+    self.resultLabel.numberOfLines = 0;
+    
+    self.beerLabel.numberOfLines = 0;
+    
 }
+
+-(void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    CGFloat viewWidth = 320;
+    CGFloat padding = 20;
+    CGFloat itemWidth = viewWidth - padding - padding;
+    CGFloat itemHeight = 44;
+    
+    self.beerPercentTextField.frame = CGRectMake(padding, padding, itemWidth, itemHeight);
+    
+    CGFloat bottomOfTextField = CGRectGetMaxY(self.beerPercentTextField.frame);
+    self.beerLabel.frame = CGRectMake(padding, bottomOfTextField, itemWidth, itemHeight);
+    
+    CGFloat bottomOfBeerLabel = CGRectGetMaxY(self.beerLabel.frame);
+    self.beerCountSlider.frame = CGRectMake(padding, bottomOfBeerLabel, itemWidth, itemHeight);
+    
+    CGFloat bottomOfBeerCountSlider = CGRectGetMaxY(self.beerCountSlider.frame);
+    self.resultLabel.frame = CGRectMake(padding, bottomOfBeerCountSlider, itemWidth, itemHeight *4);
+    
+    CGFloat bottomOfResultLabel = CGRectGetMaxY(self.resultLabel.frame);
+    self.calculateButton.frame = CGRectMake(padding, bottomOfResultLabel, itemWidth, itemHeight);
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Richie Austerberry on 8/10/2014.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
